5af846ef8203888339d5cc42aa071b65b6aca4a8
gokukkraju1993 <gokul.raju@fingent.com>
2019-05-31 15:32:26 +0530

    sdfsdf

--- WebApplication5/Controllers/ValuesController.cs
+++ WebApplication5/Controllers/ValuesController.cs
@@ -48,7 +48,7 @@ namespace WebApplication5.Controllers
         public ActionResult<string> Send(string message)
         {
 
-            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var factory = new ConnectionFactory() { HostName = "10.0.75.1:15672" };
             using (var connection = factory.CreateConnection())
             {
                 using (var channel = connection.CreateModel())
