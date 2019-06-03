ee450c01a2d8c6ea1a06fceeb8349c71b8f89379
gokukkraju1993 <gokul.raju@fingent.com>
2019-06-03 11:05:14 +0530

    fdcz

--- WebApplication5/Controllers/ValuesController.cs
+++ WebApplication5/Controllers/ValuesController.cs
@@ -48,7 +48,7 @@ namespace WebApplication5.Controllers
         public ActionResult<string> Send(string message)
         {
 
-            var factory = new ConnectionFactory() { HostName = "127.0.0.1" , Port = 5672 };
+            var factory = new ConnectionFactory() { HostName = "localhost" , Port = 86 };
             using (var connection = factory.CreateConnection())
             {
                 using (var channel = connection.CreateModel())
